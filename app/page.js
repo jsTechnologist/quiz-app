b1f4e36be55519923dffc45defb69ec95072b9e7
Ali rafie <aliarshi1812@gmail.comgit config --global user.email aliarshi1812@gmail.com>
2024-04-13 17:11:11 +0330

    add new file

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function Home(){
+    return ( 
+        <main>
+            <div className="container">
+                <h1>    اپلیکیشن   Quiz</h1>
+                <Link href= "/quiz">
+                    <button> شروع آزمون </button>
+                </Link>
+            </div>
+        </main>
+    )
+}
